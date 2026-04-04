2568eae4a79a5ef7fba5ef5891eb89c0100a5c6b
kamariban <112406892+kamariban@users.noreply.github.com>
2026-04-04 15:17:24 -0400

    Update with Advice App

--- app.py
+++ app.py
@@ -1,2 +1,11 @@
-print("Hello, World!")
+import requests
 
+def get_advice():
+    url = "https://api.adviceslip.com/advice"
+
+    response = requests.get(url)
+    data = response.json()
+
+    return data["slip"]["advice"]
+
+print(get_advice())
